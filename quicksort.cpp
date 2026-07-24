a21c100290188be9882d14fa3e94e8158e39c1a6
harsh1h2a <harsh1h2a@gmail.com>
2026-07-25 01:49:23 +0530

    Implement quicksort algorithm in C++

---
+++ quicksort.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int partition(vector<int> &arr,int st,int end){
+    int idx=st-1,pivot=arr[end];
+    for(int j=st;j<end;j++){
+        if(arr[j]<=pivot){
+            idx++;
+            swap(arr[j],arr[idx]);
+        }
+    }
+    idx++;
+    swap(arr[end],arr[idx]);
+    return idx;
+}
+
+void quicksort(vector<int>& arr,int st,int end){
+    if(st<end){
+        int pividx=partition(arr,st,end);
+        quicksort(arr,st,pividx-1);
+        quicksort(arr,pividx+1,end);
+    }
+}
+
+
+int main() {
+    vector<int> arr={2,23,54,34,11,40,39,22,30};
+    quicksort(arr,0,arr.size()-1);
+    for(int val:arr){
+        cout<<val<<"  ";
+        
+    }
+    cout<<endl;
+    return 0;
+}
