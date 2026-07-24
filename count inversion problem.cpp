ac3ce8f8fcb5ce55efa571636ad411f8d57b598e
harsh1h2a <harsh1h2a@gmail.com>
2026-07-25 02:33:31 +0530

    Implement count inversion problem using merge sort

---
+++ count inversion problem.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int merge(vector<int>&arr,int st,int mid,int end){
+    vector<int> temp;
+    int i=st,j=mid+1;
+    int invcount=0;
+    while(i<=mid && j<=end){
+        if(arr[i]<=arr[j]){
+            temp.push_back(arr[i]);
+            i++;
+        }else{
+            temp.push_back(arr[j]);
+            j++;
+            invcount +=(mid-i+1);
+        }
+    }
+    while(i<=mid){
+        temp.push_back(arr[i]);
+            i++;
+    }
+    while(j<=end){
+        temp.push_back(arr[j]);
+        j++;
+    }
+    for(int idx=0;idx<temp.size();idx++){
+        arr[idx+st]=temp[idx];
+    }
+    return invcount;
+}
+
+int mergesort(vector<int> &arr,int st,int end){
+    if(st<end){
+        int mid=st+(end-st)/2;
+        
+        int leftinversioncount = mergesort(arr,st,mid);
+        int rightinversioncount = mergesort(arr,mid+1,end);
+        int inversioncount = merge(arr,st,mid,end);
+        return leftinversioncount + rightinversioncount + inversioncount;
+    }
+    return 0;
+}
+
+int main() {
+    vector<int> arr={1,3,5,10,2,6,8,9};
+    int ans=mergesort(arr,0,arr.size()-1);
+    
+    cout<<"inversion count = "<<ans<<endl;
+    return 0;
+}
